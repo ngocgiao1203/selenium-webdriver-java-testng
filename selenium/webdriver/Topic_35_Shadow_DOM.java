c72f55d724e4a596811724db61d1926b0ec4bf42
nguyenthaingocgiao <ngocgiaok18ufm@gmail.com>
2025-01-28 03:32:49 +0700

    Merge remote-tracking branch 'origin/master'

--- selenium/webdriver/Topic_35_Shadow_DOM.java
+++ selenium/webdriver/Topic_35_Shadow_DOM.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -29,11 +30,24 @@ public class Topic_35_Shadow_DOM {
         //Thuộc DOM cha bên ngoài
         driver.findElement(By.xpath("//a[text()='scroll.html']"));
 
-        //Element cha chứa shadow host
+        //Element cha chứa Shadow Host thứ 1
         WebElement shadowHostParent = driver.findElement(By.cssSelector("div#shadow_host"));
+        //Lấy ra element Shadow root
+        SearchContext firstShadow = shadowHostParent.getShadowRoot();
 
+        Assert.assertTrue(firstShadow.findElement(By.cssSelector("input[type='file']")).isDisplayed());
+
+        Assert.assertEquals(firstShadow.findElement(By.cssSelector("span.info")).getText(),"some text");
+
+
+        //Element cha chứa shadow host thứ 2
+        WebElement firstShadowHostParent = firstShadow.findElement(By.cssSelector("div#nested_shadow_host"));
         //Lấy ra element shadow root
-        SearchContext firstShadown= shadowHostParent.getShadowRoot();
+        SearchContext secondShadow = firstShadowHostParent.getShadowRoot();
+        Assert.assertEquals(secondShadow.findElement(By.cssSelector("div#nested_shadow_content>div")).getText(),"nested text");
+
+        Assert.assertTrue(firstShadow.findElement(By.cssSelector("a[href='scroll.html']")).isDisplayed());
+        driver.findElement(By.xpath("//a[text()='scroll.html']"));
     }
 
 
